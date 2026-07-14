87024b4d59e11b20ff12731205034b94c927ace7
RutujaBidkar <Rutujadbidkar@gmail.com>
2026-07-14 22:44:42 +0530

    Swap Variables

---
+++ Python/Swap_Number.py
@@ -0,0 +1,13 @@
+# Input two variable
+a = Input("Enter the value of the first variable (a) :")
+b = Input("Enter the value of the second variable (b) :")
+
+#Display the original Value
+print(f"Original Values : a = {a}, b = {b}")
+#swap the variable
+temp = a
+a = b
+b = temp
+
+print(f"Original Values : a = {a}, b = {b}")
+
