721a510da98f9c182754eee5f5fe715e9826848e
RutujaBidkar <Rutujadbidkar@gmail.com>
2026-07-17 13:22:04 +0530

    Python Query

---
+++ Python/string.py
@@ -0,0 +1,29 @@
+#s1 = "spring"
+
+#for x in s1:
+ #   print(x)
+
+s2= "java python"
+#print(len(s2))
+
+#for x in range(1,len(s2),2):
+   # print(x)
+
+for x in range(0,len(s2),2):
+    print(s2[-3])
+
+s1 = 'priya'
+print(s1[:])
+
+> print(s1[:])
+print(s1[:len(s1):-1])
+
+
+
+
+
+
+
+
+
+
